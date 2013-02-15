d368bcd60f57b02ecc79f0f66def9ae13339861f
Mike Emsley <mike.emsley@gmail.com>
2013-02-15 17:25:49 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedbackhero::Application.config.secret_token = '3770cb26d832dfae9fdb3b655602302c0e700679ccfcf9cfbfafc7dab1d7dd0895fbafe32f531f2bd05889b1507c85285a0968815e29686fbb92c3e1b50ef336'
