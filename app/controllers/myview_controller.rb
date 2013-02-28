1eb250bbcc36ec69ba874f1ebdd5fa5801dbae08
Mike Emsley <mike.emsley@gmail.com>
2013-02-28 15:07:30 -0600

    badges sent

--- app/controllers/myview_controller.rb
+++ app/controllers/myview_controller.rb
@@ -2,7 +2,9 @@ class MyviewController < ApplicationController
   def index
   	u = current_user
   	@feedbacks = u.feedbacks.all
-  	#@userbadges = u.user_badge.all
+  	@userbadges = u.user_badges.all
+  	@feedbackleft = Feedback.where(:from_user => current_user)
+  	@badgesent = UserBadge.where(:from_user => current_user)
   end
 
   def edit
