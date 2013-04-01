c85d701b930944de6b845425d4c56bc8d4a94237
Mike Emsley <mike.emsley@gmail.com>
2013-04-01 07:21:32 -0500

    user projects

--- app/controllers/userprojects_controller.rb
+++ app/controllers/userprojects_controller.rb
@@ -1,13 +1,52 @@
 class UserprojectsController < ApplicationController
   def index
+  	@userprojects = Userproject.all
   end
 
   def show
+  	@userproject = Userproject.find(params[:id])
   end
 
   def edit
+  	@userproject = Userproject.find(params[:id])
   end
 
   def new
+  	@userproject = Userproject.new
   end
+
+  def create
+	@userproject = Userproject.new(params[:userproject])
+	if @userproject.save
+		redirect_to @userproject
+	else
+		render :edit
+	end
+  end
+
+  def update
+	@userproject = Userproject.find(params[:id])
+	if @userproject.update_attributes(params[:userproject])
+		redirect_to @userproject
+	else
+		render :edit
+	end
+  end
+
+  def destroy
+	@userproject = Userproject.find(params[:id])
+	@userproject_id = @userproject.id
+	if @userproject.destroy
+		redirect_to @userproject
+	else
+		flash[:error] = "Error deleting user project"
+		redirect_to @userproject
+	end
+
+  end
+
+
+
+
+
 end
--- app/models/userproject.rb
+++ app/models/userproject.rb
@@ -1,5 +1,5 @@
 class Userproject < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
-  # attr_accessible :title, :body
+  attr_accessible :user_id, :project_id
 end
